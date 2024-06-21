7b3844909c84fc4e42372e0c6c55c729d36c5543
Adithya Krishnan <adiadithyakrishnan@gmail.com>
2024-06-21 12:08:00 +0530

    fix : build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 "use client"
-import Image from "next/image";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
-  router.push("/signup")
+
+  useEffect(() => {
+    router.push("/signup");
+  }, [router]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 ">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-[5vw]">Tuples</h1>
       <h2 className="text-[1vw]">Coming Soon</h2>
     </main>
