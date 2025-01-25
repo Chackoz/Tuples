d581efce5cf88212a3b57cd686ba34e013802418
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2025-01-25 13:39:59 +0530

    fix : build issues tailwind + shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,10 +10,7 @@ const config: Config = {
   theme: {
   	extend: {
   		colors: {
-  			'primary': '#2d2d2d',
-  			'secondary': '#3a5a8c',
-  			'accent': '#f9f8ea',
-  			'background': '#fdfdfd',
+  			
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
